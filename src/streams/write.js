4013b78e8fa528be303fb15e9de061c3cf3a8856
Maryia Piarshai <mary.pershay@gmail.com>
2023-06-16 18:30:45 +0200

    feat: add streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs';
+import * as path from "path";
+import { fileURLToPath } from 'url';
+import {stdin, stdout} from "process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, `/files/fileToWrite.txt`);
+
+
 const write = async () => {
-    // Write your code here 
+    const output = fs.createWriteStream(filePath, { encoding: 'utf-8' });
+    stdout.write('Please, write something in console\n');
+    stdin.on('data', data => {
+        output.write(data);
+        process.exit();
+    });
 };
 
 await write();
