f8e2d28aefb6dc8cf6ccdfa96cf4920ddd2c282c
Maryia Piarshai <mary.pershay@gmail.com>
2023-06-16 12:11:47 +0200

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const content = 'I am fresh and young';
+const fileName = 'fresh.txt'
+const errorMessage = 'FS operation failed';
+let filePath = path.join(__dirname, `/files/${fileName}`);
+
 const create = async () => {
-    // Write your code here 
+    fs.writeFile(filePath, content, { flag: 'ax' }, err => {
+        if (err) {
+            throw Error(errorMessage);
+        }
+        console.log(`File ${fileName} was created with text -  ${content}`);
+    });
 };
 
 await create();
