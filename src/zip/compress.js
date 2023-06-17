2ac1618b7dcd555b7f1cd255757588140699bd51
Maryia Piarshai <mary.pershay@gmail.com>
2023-06-17 08:23:24 +0200

    feat: update zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -6,10 +6,11 @@ import zlib from "zlib";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, `/files/fileToCompress.txt`);
+const fileCompressedPath = path.join(__dirname, `/files/archive.gz`);
 
 const compress = async () => {
     const readStream = fs.createReadStream(filePath, 'utf-8');
-    const writeStream = fs.createWriteStream(filePath);
+    const writeStream = fs.createWriteStream(fileCompressedPath);
     const compressStream = zlib.createGzip();
 
     readStream
