aef40c8b370983bebb10fa703761df1f13ea8d93
Maryia Piarshai <mary.pershay@gmail.com>
2023-06-17 08:20:58 +0200

    feat: add zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import * as fs from 'node:fs';
+import {fileURLToPath} from "url";
+import path from "path";
+import zlib from "zlib";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, `/files/fileToCompress.txt`);
+
 const compress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(filePath, 'utf-8');
+    const writeStream = fs.createWriteStream(filePath);
+    const compressStream = zlib.createGzip();
+
+    readStream
+        .pipe(compressStream)
+        .pipe(writeStream);
 };
 
 await compress();
