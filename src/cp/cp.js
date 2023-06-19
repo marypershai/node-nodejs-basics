1277aa7f6b515cf0bfd22a0530a65010023bbbbd
Maryia Piarshai <mary.pershay@gmail.com>
2023-06-19 10:02:33 +0200

    feat: add cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { fileURLToPath } from 'node:url';
+import { spawn } from 'node:child_process';
+import * as path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, `/files/script.js`);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+
+    spawn('node', [filePath, ...args], {
+        stdio: [process.stdin, process.stdout, 'ipc'],
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2', 12]);
